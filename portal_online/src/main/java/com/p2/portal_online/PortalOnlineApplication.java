f77a9f7146ba568d6c68349510a6db7ff4a75aa0
Adrián Benítez Roldán <149155965+AdrianBR003@users.noreply.github.com>
2025-02-26 09:49:55 +0100

    first commit
    + form-login.html
    + form-register.html

---
+++ portal_online/src/main/java/com/p2/portal_online/PortalOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.p2.portal_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalOnlineApplication.class, args);
+	}
+
+}
---
+++ portal_online/src/test/java/com/p2/portal_online/PortalOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p2.portal_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
